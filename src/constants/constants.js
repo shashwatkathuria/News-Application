738d30c76918ed0636404aaad901deba1478a0aa
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-10-22 00:31:16 +0530

    Added constants required.

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const FETCH_NEWS_ARTICLES = 'FETCH_NEWS_ARTICLES';
+export const SAVE_NEWS_ARTICLE = 'SAVE_NEWS_ARTICLE';
+export const UNSAVE_NEWS_ARTICLE = 'UNSAVE_NEWS_ARTICLE';
+
+export const ADD_FILTER = 'ADD_FILTER';
+export const REMOVE_FILTER = 'REMOVE_FILTER';
+export const CLEAR_FILTERS = 'CLEAR_FILTERS';
+
+export const NEWS_API_URL = 'http://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=5154c00fe4264a80bdc7db81cb5d8cdc';
