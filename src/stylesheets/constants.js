0c987155b362c3cf7eeedc5f81e45bddb370696e
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-10-27 02:09:28 +0530

    Added UI constants to be used universally.

---
+++ src/stylesheets/constants.js
@@ -0,0 +1,24 @@
+export const FONT_FAMILY = 'sans-serif';
+
+export const NEWS_ITEM_FONT_WEIGHT = 'bold';
+
+export const BORDER_RADIUS = 4;
+
+export const MARGIN_SMALL = 4;
+export const MARGIN_MEDIUM = 8;
+export const MARGIN_LARGE = 16;
+
+export const CLICK_OPACITY = 0.7;
+export const OPACITY_BACKGROUND = 'rgba(0, 0, 0, 0.5)';
+
+export const BACKGROUND_COLOR = '#F3F2F0';
+export const INPUT_BACKGROUND = '#F1F1F1';
+export const STANDARD_LIGHT = '#FFFFFF';
+export const STANDARD_MEDIUM = '#DDDDDD';
+export const STANDARD_DARK = '#000000';
+export const THEME = 'purple';
+
+export const LOADING_SPINNER_SIZE = 10;
+
+export const NEWS_ITEM_LINES_LIMIT = 4;
+export const METADATA_LINES_LIMIT = 1;
