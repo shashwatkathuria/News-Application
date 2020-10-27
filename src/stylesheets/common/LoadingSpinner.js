f9a1dd9bc35a2ece2ed4537ae15a42f82b8fefc4
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-10-28 02:09:17 +0530

    Styling for loading spinner.

---
+++ src/stylesheets/common/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import * as constants from '../constants';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  }
+});
+
+export default styles;
