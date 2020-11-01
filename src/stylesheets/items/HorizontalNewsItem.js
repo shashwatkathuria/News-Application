aef599dad7649ed61c95bc40ec9376da7c3b2381
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-11-01 21:56:06 +0530

    Specific styling to this particular element.

---
+++ src/stylesheets/items/HorizontalNewsItem.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import * as constants from '../constants';
+
+const styles = StyleSheet.create({
+  newsImage: {
+    width: 130,
+    height: 100,
+    borderRadius: constants.BORDER_RADIUS
+  }
+});
+
+export default styles;
