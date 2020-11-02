d583dcc3c2c8a21e5def8f29953df044c22f6be4
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-11-03 00:02:19 +0530

    Styling for horizontal carousel.

---
+++ src/stylesheets/featured/NewsCarousel.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import * as constants from '../constants';
+
+const styles = StyleSheet.create({
+  carouselContainer: {
+    marginLeft: constants.MARGIN_LARGE,
+    paddingTop: constants.MARGIN_MEDIUM,
+    paddingRight: constants.MARGIN_LARGE
+  },
+});
+
+export default styles;
