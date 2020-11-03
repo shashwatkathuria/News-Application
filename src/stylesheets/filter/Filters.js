d82b1f1de9ae544c005e782cfc7afab254090041
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-11-04 01:16:24 +0530

    Common styling for filters.

---
+++ src/stylesheets/filter/Filters.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import * as constants from '../constants';
+
+const styles = StyleSheet.create({
+  clearAllText: {
+    color: constants.THEME,
+    textDecorationLine: 'underline',
+    fontFamily: constants.FONT_FAMILY
+  }
+});
+
+export default styles;
