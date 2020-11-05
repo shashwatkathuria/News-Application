e59f5bcd4e178c1c6a63ad632fef286f6602ba2d
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-11-06 01:17:30 +0530

    Single point of contact of export to src/components/.

---
+++ src/stylesheets/main.js
@@ -0,0 +1,47 @@
+import * as constants from './constants';
+
+import newsFeedScreenStyles from './screens/NewsFeedScreen';
+import savedNewsScreenStyles from './screens/SavedNewsScreen';
+import webViewScreenStyles from './screens/WebViewScreen';
+
+import commonNewsItemStyles from './items/NewsItem';
+import largeNewsCardItemStyles from './items/VerticalNewsItem';
+import rowTextImageNewsItemStyles from './items/HorizontalNewsItem';
+
+import newsCarouselStyles from './featured/NewsCarousel';
+import newsBackgroundItemStyles from './featured/NewsBackgroundItem';
+import newsHeadlineItemStyles from './featured/NewsHeadlineItem';
+import newsCarouselItemStyles from './featured/NewsCarouselItem';
+
+import commonFilterStyles from './filter/Filters';
+import filterBarStyles from './filter/bar/FilterBar';
+import filterPillStyles from './filter/bar/FilterPill';
+import filterMenuStyles from './filter/menu/FilterMenu';
+import filterOptionStyles from './filter/menu/FilterOption';
+
+import newsItemModalStyles from './common/NewsItemModal';
+import loadingSpinnerStyles from './common/LoadingSpinner';
+
+import newsPillStyles from './common/NewsPill';
+
+export {
+  newsFeedScreenStyles,
+  savedNewsScreenStyles,
+  webViewScreenStyles,
+  newsHeadlineItemStyles,
+  loadingSpinnerStyles,
+  largeNewsCardItemStyles,
+  rowTextImageNewsItemStyles,
+  filterBarStyles,
+  filterPillStyles,
+  filterMenuStyles,
+  filterOptionStyles,
+  newsCarouselStyles,
+  newsCarouselItemStyles,
+  newsItemModalStyles,
+  newsPillStyles,
+  commonFilterStyles,
+  newsBackgroundItemStyles,
+  commonNewsItemStyles,
+  constants
+};
